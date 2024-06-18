976c8261138304b0a6c36d949ca7063ea268ecf7
Gabriela Santana <gabriela2santtanna@gmail.com>
2024-06-17 21:32:25 -0300

    Add files via upload

---
+++ eleicao/src/view/UrnaView.java
@@ -0,0 +1,95 @@
+package view;
+
+import model.Urna;
+import java.time.LocalDate;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class UrnaView {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			UrnaView window = new UrnaView();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(586, 419);
+		shell.setText("SWT Application");
+		
+		Urna urna = new Urna(1);
+		
+		DateTime dateTimeAbertura = new DateTime(shell, SWT.BORDER);
+		dateTimeAbertura.setBounds(30, 63, 229, 34);
+		
+		DateTime dateTimeFechamento = new DateTime(shell, SWT.BORDER);
+		dateTimeFechamento.setBounds(307, 63, 229, 34);
+		
+		Button btnAbertura = new Button(shell, SWT.NONE);
+		btnAbertura.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				urna.abrirUrna(LocalDate.now());
+				
+				// date time pega 1 mês a frente
+				dateTimeAbertura.setDay(urna.getDataAbertura().getDayOfMonth());
+				dateTimeAbertura.setMonth(urna.getDataAbertura().getMonthValue());
+				dateTimeAbertura.setYear(urna.getDataAbertura().getYear());
+			}
+		});
+		btnAbertura.setBounds(82, 154, 97, 34);
+		btnAbertura.setText("Abrir Urna");
+		
+		Button btnFechamento = new Button(shell, SWT.NONE);
+		btnFechamento.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				urna.fecharUrna(LocalDate.now());
+				
+				// date time pega 1 mês a frente
+				dateTimeFechamento.setDay(urna.getDataAbertura().getDayOfMonth());
+				dateTimeFechamento.setMonth(urna.getDataAbertura().getMonthValue());
+				dateTimeFechamento.setYear(urna.getDataAbertura().getYear());
+				
+			}
+		});
+		btnFechamento.setBounds(372, 154, 97, 34);
+		btnFechamento.setText("Fechar Urna");
+		
+		
+		
+	}
+}
